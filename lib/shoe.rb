6e6f59f4c55c94b96959088404e38f1dad4d10c9
Chayan Martinez <nayachmartinez68@gmail.com>
2020-01-08 06:45:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,36 +1,48 @@
 # Make your shoe class here!
 class Shoe
+  def initialize(brand = "Nike")
+   @brand = brand
+  end
   
-attr_accessor :brand = "Nike", :color = "red", :size = 9.5, :material = "suede", :condition = "old"
-  #def initialize(brand = "Nike")
-   # @brand = brand
-  #end
+  def brand=(brand = "Nike")
+    @brand = brand
+  end
   
-#  def brand=(brand = "Nike")
- #   @brand = brand
-  #end
+  def brand
+    @brand
+  end
   
-#  def brand
- #   @brand
-#  end
+  def color=(color = "red")
+    @color = color
+  end
   
- # def color=(color = "red")
-  #  @color = color
-  #end
+  def color
+    @color
+  end
   
-  #def color
-  #  @color
-  #end
+  def size=(size = 9.5)
+    @size = size
+  end
   
-  #def size=(size = 9.5)
-  #  @size = size
-  #end
+  def size
+    @size
+  end
   
-  #def size
-  #  @size
-  #end
+  def material=(material = "suede")
+    @material = material
+  end
   
-  #def material
+  def material
+    @material
+  end
+  
+  def condition=(condition = "old")
+    @conditon = conditon
+  end
+  
+  def conditon
+    @condition
+  end
   
   
   
